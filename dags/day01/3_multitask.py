c494e45cf7d14142b4140a54c14896827c5649d1
johney <johney@aazads.us>
2023-07-26 16:03:17 +0000

    working dags from vm

---
+++ dags/day01/3_multitask.py
@@ -0,0 +1,32 @@
+import datetime
+
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+with DAG(
+    "03_multi_task_dag",
+    schedule=None, 
+    start_date=datetime.datetime(2023,6,6)
+) as dag:
+
+    bash_task = BashOperator(
+        task_id = "bash_task",
+        bash_command = "echo This DAG is using BASH Operator"
+    )
+
+    def calculate (a,b):
+        c = a + b
+        print(c)
+        return c
+        
+    python_task = PythonOperator(
+        task_id = "python_task",
+        python_callable = calculate,
+        op_kwargs = {
+            'a': '1',
+            'b': '2'
+        }
+    )
+
+    python_task >> bash_task
